59781d098528f8238fc66407da4d7272f656c4ba
Anastasiya_Sheptur <nasya2611@gmail.com>
2018-03-04 00:17:20 +0200

    11 Task is done

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,36 @@
+class Dictionary
+  attr_accessor :entries
+
+  def initialize
+    self.entries = {}
+  end
+
+  def add(a)
+    if a.is_a? Hash
+      entries.merge!(a)
+    else
+      entries[a] = nil
+    end
+  end
+
+  def keywords
+    entries.keys.sort
+  end
+
+  def include?(k)
+    entries.has_key?(k)
+  end
+
+  def find(str)
+    entries.select {|k,v| k =~ /^#{str}/ }
+  end
+
+  def printable
+    str = []
+    new_hash = entries.sort_by { |k, v| k}
+    new_hash.each do |k,v|
+      str << "[#{k}] \"#{v}\"" 
+    end
+    str.join("\n")
+  end
+end
